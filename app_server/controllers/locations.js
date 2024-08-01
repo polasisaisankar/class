501bb69a67ec98b12400efeccedce0dcf962feea
sai sankar <polasisaisankar@gmail.com>
2024-08-01 14:12:35 +0530

    first commit

---
+++ app_server/controllers/locations.js
@@ -0,0 +1,17 @@
+/* GET 'home' page */
+const homelist = (req, res) => {
+    res.render('locations-list', { title: 'Home' });
+   };
+   /* GET 'Location info' page */
+   const locationInfo = (req, res) => {
+    res.render('location-info', { title: 'Location info' });
+   };
+   /* GET 'Add review' page */
+   const addReview = (req, res) => {
+    res.render('location-review-form', { title: 'Add review' });
+   };
+   module.exports = {
+    homelist,
+    locationInfo,
+    addReview
+   };
---
+++ app_server/controllers/main.js
@@ -0,0 +1,4 @@
+/* GET homepage */
+const index = (req, res) => {
+    res.render('index', { title: 'Express' });
+   };
